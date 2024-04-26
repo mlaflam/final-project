628d0d2a134b68f5950fcdc866ffbb7044d4eb01
mlaflam <mlaflam@users.noreply.github.com>
2024-04-26 13:31:03 -0400

    Adding Plant Interaction

--- Button.gd
+++ Button.gd
@@ -4,4 +4,3 @@ extends Button
 
 func _on_pressed(): # make mirror panel visible dont want it to go back and forth 
 	RoomManager.isMirrorOn = true
-	RoomManager.wasPressed = true
--- room_manager.gd
+++ room_manager.gd
@@ -4,4 +4,6 @@ var wasBath : bool = false
 
 var isMirrorOn : bool = false 
 
+var isPlantOn : bool = false 
+
 var wasPressed : bool = false 
